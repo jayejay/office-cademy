3e722f08989a4e31eeff7281cbfa76061dd2b9bf
Julian Eissing <julianeissing@web.de>
2017-10-03 13:55:36 +0200

    added migration to change columnname status to active

--- resources/views/posts/partials/form.blade.php
+++ resources/views/posts/partials/form.blade.php
@@ -1,6 +1,9 @@
 {{ csrf_field() }}
-<textarea class="form-control" name="body" id="textarea" cols="100" rows="10">{{ old('body', $post->body) }}</textarea>
-<button class="btn btn-success" id="send">Absenden</button>
+<label for="title">Post title</label>
+<input type="text" id="title" name="title" class="form-control" placeholder="Enter post title">
+<label for="body">Post body</label>
+<textarea class="form-control" name="body" id="body" placeholder="Enter post body" cols="100" rows="15">{{ old('body', $post->body) }}</textarea>
+<button type="submit" class="btn btn-success" id="send">Save</button>
 <button id="preview_button" type="button" class="btn btn-warning pull-right" data-toggle="modal" data-target="#preview">Preview</button>
 
             
--- resources/views/posts/partials/form_html_helper.blade.php
+++ resources/views/posts/partials/form_html_helper.blade.php
@@ -25,11 +25,11 @@
 
                         <a href="#" data-html='<p></p>' class="btn btn-primary btn-sm html_tags">Paragraph</a>
                         <a href="#" data-html='<div class="" id="" name=""></div>' class="btn btn-primary btn-sm html_tags">Div</a>
-                        <a href="#" data-html='@include('posts/parts/template_ordered_list')' class="btn btn-primary btn-sm html_tags">Ordered List</a>
+                        <a href="#" data-html='@include('posts/partials/template_ordered_list')' class="btn btn-primary btn-sm html_tags">Ordered List</a>
                     </li>
                     <li>
-                        <a href="#" data-html='@include('posts/parts/template_unordered_list')' class="btn btn-primary btn-sm html_tags">Unordered List</a>
-                        <a href="#" data-html= '@include('posts/parts/template_table')' class="btn btn-primary btn-sm html_tags">Table</a>
+                        <a href="#" data-html='@include('posts/partials/template_unordered_list')' class="btn btn-primary btn-sm html_tags">Unordered List</a>
+                        <a href="#" data-html= '@include('posts/partials/template_table')' class="btn btn-primary btn-sm html_tags">Table</a>
                     </li>
                 </ul>
             </div> 
