c708898d48f2d04eb71ffff7581417c053578cb1
Jay <julianeissing@web.de>
2017-11-30 20:44:12 +0100

    fixed template translations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,9 +45,9 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
                 &nbsp;
-                        <li><a href="{{route('tags.admin.index')}}">@lang('Custom.Tags')</a></li>
-                        <li><a href="{{route('chapters.admin.index')}}">@lang('Custom.Chapters')</a></li>
-                        <li><a href="{{route('courses.admin.index')}}">@lang('Custom.Courses')</a></li>
+                        <li><a href="{{route('tags.admin.index')}}">@lang('custom.Tags')</a></li>
+                        <li><a href="{{route('chapters.admin.index')}}">@lang('custom.Chapters')</a></li>
+                        <li><a href="{{route('courses.admin.index')}}">@lang('custom.Courses')</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
