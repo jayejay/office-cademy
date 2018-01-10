5ea0fd09d6f3a2fdbe86bdb9490f1f9bf12857f5
Jay <julianeissing@web.de>
2018-01-10 12:27:29 +0100

    doing fix routes for navigation

--- resources/views/layouts/partials/excel_course_dropdown.blade.php
+++ resources/views/layouts/partials/excel_course_dropdown.blade.php
@@ -5,7 +5,7 @@
     <li data-sm-reverse="true">
         <a href="#">@lang('custom.Formulas') <span class="caret"></span></a>
         <ul class="dropdown-menu">
-            <li><a href="#">Action1</a></li>
+            <li><a href="{{route('posts.show', 2)}}">Intro und Aufbau</a></li>
             <li><a href="#">Action2</a></li>
             <li><a href="#">Action3</a></li>
         </ul>
