466a6c604bacd656e21b37ede7b6589a1ce3d4b6
Jay <julianeissing@web.de>
2018-01-21 20:38:10 +0100

    test perfect scrollbar, added update

--- resources/views/layouts/partials/head/dashboard_kit_js.blade.php
+++ resources/views/layouts/partials/head/dashboard_kit_js.blade.php
@@ -2,6 +2,6 @@
 <script src="{{asset('js/vendor/creativetim/bootstrap.min.js')}}" type="text/javascript"></script>
 <!--   Core JS Files from Creative Tim -->
 <script src="{{asset('js/vendor/creativetim/material.min.js')}}"></script>
-<script src="{{ asset('js/vendor/creativetim/perfect-scrollbar.jquery.min.js') }}"></script>
-<script src="{{ asset('js/vendor/creativetim/jquery.select-bootstrap.js') }}"></script>
-<script src="{{ asset('js/vendor/creativetim/material-dashboard.js') }}"></script>
+<script src="{{asset('js/vendor/creativetim/perfect-scrollbar.jquery.min.js')}}"></script>
+<script src="{{asset('js/vendor/creativetim/jquery.select-bootstrap.js')}}"></script>
+<script src="{{asset('js/vendor/creativetim/material-dashboard.js')}}"></script>
